881446d8d8465556d11913bd0659ec22d4af08e4
Christopher Mueller <cmueller@asascience.com>
2010-12-01 17:17:39 -0500

    Added overridded constructor, exposed fields to subclasses, added dispose method

--- src/ion/core/BaseProcess.java
+++ src/ion/core/BaseProcess.java
@@ -9,46 +9,61 @@ import ion.core.messaging.MsgBrokerClient;
  *
  * @author mmeisinger
  * @author Stephen Pasco
- *
+ * @author Chris Mueller
  */
 public class BaseProcess {
 
-	MessagingName mProcessId;
-	MsgBrokerClient mBrokerClient;
-	String mInQueue;
-	
-	public BaseProcess(MsgBrokerClient brokercl) {
-		mBrokerClient = brokercl;
-		mProcessId = MessagingName.generateUniqueName();
-	}
-	
-	public void spawn() {
-		mInQueue = mBrokerClient.declareQueue(null);
+    protected MessagingName mProcessId;
+    protected MsgBrokerClient mBrokerClient;
+    protected String mInQueue;
+
+    public BaseProcess(String host, int portNumber, String exchangeTopic) {
+        this(new MsgBrokerClient(host, portNumber, exchangeTopic));
+    }
+
+    public BaseProcess(MsgBrokerClient brokercl) {
+        mBrokerClient = brokercl;
+        mProcessId = MessagingName.generateUniqueName();
+    }
+
+    public void spawn() {
+        mInQueue = mBrokerClient.declareQueue(null);
         mBrokerClient.bindQueue(mInQueue, mProcessId, null);
         mBrokerClient.attachConsumer(mInQueue);
         System.out.println("Spawned process " + mProcessId);
-	}
-	
-	public void init() {
-		
-	}
-	
-	public void send(MessagingName to, String op, Object content) {
-		mBrokerClient.createSendMessage(mProcessId, to, op, content);
-	}
+    }
 
-	public IonMessage rpcSend(MessagingName to, String op, Object content) {
-		mBrokerClient.createSendMessage(mProcessId, to, op, content);
+    public void dispose() {
+        mBrokerClient.detach();
+    }
+
+    public String getInQueue() {
+        return mInQueue;
+    }
+
+    public MessagingName getMessagingName() {
+        return mProcessId;
+    }
+
+    public void init() {
+    }
+
+    public void send(MessagingName to, String op, Object content) {
+        mBrokerClient.createSendMessage(mProcessId, to, op, content);
+    }
+
+    public IonMessage rpcSend(MessagingName to, String op, Object content) {
+        mBrokerClient.createSendMessage(mProcessId, to, op, content);
         IonMessage msgin = mBrokerClient.consumeMessage(mInQueue);
         return msgin;
-	}
+    }
 
-	public IonMessage receive() {
+    public IonMessage receive() {
         IonMessage msgin = mBrokerClient.consumeMessage(mInQueue);
         return msgin;
-	}
-	
-	public void ackMessage(IonMessage msg) {
-		mBrokerClient.ackMessage(msg);
-	}
+    }
+
+    public void ackMessage(IonMessage msg) {
+        mBrokerClient.ackMessage(msg);
+    }
 }
