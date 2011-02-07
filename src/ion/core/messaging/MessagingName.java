593e4a8c4b7968839a033bfe1480f873de2a8c77
Christopher Mueller <cmueller@asascience.com>
2011-02-07 12:33:47 -0500

    Tried to mimic the generation of the MessagingName for the "localhost" as it is done on the python side.  Ran into an issue in that there are 4 numbers appended on the python side and I'm not sure where they come from...

--- src/ion/core/messaging/MessagingName.java
+++ src/ion/core/messaging/MessagingName.java
@@ -4,13 +4,23 @@ import java.util.UUID;
 
 public class MessagingName {
 	private String mName;
+//    private static String localHostName;
+//    static {
+//        try {
+//            localHostName = java.net.InetAddress.getLocalHost().getHostName();
+//            System.out.println("localHostName: " + localHostName);
+//        } catch (UnknownHostException ex) {
+//            Logger.getLogger(MessagingName.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
 	
 	public MessagingName(String qualifier, String name) {
 		this(qualifier + "." + name);
 	}
 
 	public MessagingName(String name) {
-		mName  = name;
+//		mName = localHostName + "." + name;
+        mName = name;
 	}
 
 	public String getName() {
